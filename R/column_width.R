6590b63eb5310b0ddc3cda84f69f2ee42a08d5cb
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-07-04 23:23:04 +0200

    test the inputvalues of column_width() and column_start()

--- R/column_width.R
+++ R/column_width.R
@@ -2,7 +2,18 @@
 #' @param ncol the required number of columns
 #' @param space the space in between two columns in mm
 #' @export
+#' @importFrom assertthat is.count is.number
 column_width <- function(ncol = 3, space = 20){
+  if (!is.count(ncol)) {
+    stop("ncol must be a single integer")
+  }
+  if (!is.number(space)) {
+    stop("space must be a single number")
+  }
+  if (space < 0) {
+    stop("space must be positive")
+  }
+  
   outer.border <- 20 # should match the \outerborder from beamerthemeciposter.sty
   inner.border <- 30 # should match the \outerborder from beamerthemeciposter.sty
   (839.6 - 2 * (outer.border + inner.border) - (ncol - 1) * space) / ncol
@@ -12,7 +23,15 @@ column_width <- function(ncol = 3, space = 20){
 #' @param start the number of the columns
 #' @inheritParams column_width
 #' @export
+#' @importFrom assertthat is.count
 column_start <- function(ncol = 3, start = 1, space = 20){
+  if (!is.count(start)) {
+    stop("start must be a single integer")
+  }
   width <- column_width(ncol = ncol, space = space)
+  if (start > ncol) {
+    warning("start > ncol. start = ncol used instead")
+    start <- ncol
+  }
   (start - 1) * (width + space)
 }
